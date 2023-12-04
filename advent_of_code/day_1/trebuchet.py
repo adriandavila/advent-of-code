98d850c9f28e86d8db70050f84a90a5e4f806f35
Adrian Davila <akdavila03@gmail.com>
2023-12-03 21:14:50 -0500

    `day_1`: Trebuchet!? part 2

--- advent_of_code/day_1/trebuchet.py
+++ advent_of_code/day_1/trebuchet.py
@@ -1,33 +1,90 @@
 from pathlib import Path
+from typing import Literal
 
-def trebuchet(lines: list[str]) -> int:
-    def process_line(line: str) -> int:
-        # Find first char
-        calibration_value = 0
-        for ch in line:
-            if ch.isnumeric():
-                calibration_value = int(ch) * 10
-                break
-        
-        for ch in reversed(line):
-            if ch.isnumeric():
-                calibration_value += int(ch)
-                break
+def process_line(line: str) -> int:
+    calibration_value = 0
+    # Find the first number
+    for ch in line:
+        if ch.isnumeric():
+            calibration_value = int(ch) * 10
+            break
+    # Find the second number
+    for ch in reversed(line):
+        if ch.isnumeric():
+            calibration_value += int(ch)
+            break
 
-        return calibration_value
+    return calibration_value
 
+
+
+def process_line_v2(line: str) -> int:
+    valid_entries: dict[str, int] = {
+        "one": 1,
+        "two": 2,
+        "three": 3,
+        "four": 4,
+        "five": 5,
+        "six": 6,
+        "seven": 7,
+        "eight": 8,
+        "nine": 9,
+    }
+
+
+    calibration_value = 0
+    # Find the first number
+    char_found = False
+    for ch_idx in range(len(line)):
+        # Test for a number
+        if line[ch_idx].isnumeric():
+            calibration_value = int(line[ch_idx]) * 10
+            char_found = True
+
+        # Check for all the words
+        for i in range(1, min(len(line) - ch_idx, 6)):
+            substr = line[ch_idx:ch_idx + i]
+            if substr in valid_entries:
+                calibration_value = valid_entries[substr] * 10
+                char_found = True
+
+        if char_found: break
+    
+    char_found = False
+
+    # Find the second number
+    for ch_idx in range(len(line) - 1, -1, -1):
+        # Test for a number
+        if line[ch_idx].isnumeric():
+            calibration_value += int(line[ch_idx])
+            char_found = True
+
+        # Check for all the words
+        for i in range(1, min(len(line) - ch_idx + 1, 6)):
+            substr = line[ch_idx:ch_idx + i]
+            if substr in valid_entries:
+                calibration_value += valid_entries[substr]
+                char_found = True
+
+        if char_found: break
+
+    return calibration_value
+
+def trebuchet(lines: list[str], version: Literal["v1"] | Literal["v2"] = "v1") -> int:
     acc: int = 0
-
     for line in lines:
-        acc += process_line(line)
+        if version == "v1":
+            acc += process_line(line)
+        elif version == "v2":
+            acc += process_line_v2(line)
 
     return acc
 
 
-def runner(input: Path) -> int:
+def runner(input: Path, version: Literal["v1"] | Literal["v2"] = "v1") -> int:
     lines: list[str] = []
 
     with input.open("r") as file:
         lines = [l.rstrip() for l in file]
     
-    return trebuchet(lines)
+    return trebuchet(lines, version)
