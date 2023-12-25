62d49c56baf4881136deeec255e0312535aadf81
Adrian Davila <akdavila03@gmail.com>
2023-12-25 01:57:41 -0500

    [day_05] part 2

--- 2023/day_05/test_seed_fertilizer.py
+++ 2023/day_05/test_seed_fertilizer.py
@@ -1,6 +1,7 @@
 from common.utils import get_input_dir
 
 from day_05.part1 import runner as seed_fertilizer_p1
+from day_05.part2 import runner as seed_fertilizer_p2
 
 test_dir = get_input_dir(__file__)
 
@@ -9,4 +10,9 @@ def test_seed_fertilizer_p1():
     assert seed_fertilizer_p1(test_dir / "sample.txt") == 35
 
 
+def test_seed_fertilizer_p2():
+    assert seed_fertilizer_p2(test_dir / "sample.txt") == 46
+
+
 print(seed_fertilizer_p1(test_dir / "input.txt"))
+print(seed_fertilizer_p2(test_dir / "input.txt"))
