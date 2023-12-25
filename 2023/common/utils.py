ea454b6e99f6d0808dfa0c35c2e23b6c911b6aad
Adrian Davila <akdavila03@gmail.com>
2023-12-25 01:43:08 -0500

    [common] common module

--- 2023/common/utils.py
+++ 2023/common/utils.py
@@ -2,4 +2,4 @@ from pathlib import Path
 
 def get_input_dir(file: str) -> Path:
     day, year = Path(file).parent.name, Path(file).parent.parent.name
-    return Path(__file__).parent.parent / "aoc-inputs" / year / day
+    return Path(__file__).parent.parent.parent / "aoc-inputs" / year / day
